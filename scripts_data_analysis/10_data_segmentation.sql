528721fa6555841f2b7212b337dfdcb4a6b8b714
Chawin Gump <168575437+ChawinHong@users.noreply.github.com>
2025-12-28 13:36:51 +0700

    Add data segmentation for product cost analysis
    This SQL script segments products into cost ranges and counts the number of products in each segment.

--- scripts_data_analysis/10_data_segmentation.sql
+++ scripts_data_analysis/10_data_segmentation.sql
@@ -1,1 +1,28 @@
+-- Data Segmentation
+/*
+Segment products into cost ranges and count how many products fall into each segment
+*/
+
+SELECT *
+FROM gold.dim_products
+
+
+WITH product_segments AS (
+SELECT
+	product_key,
+	product_name,
+	cost,
+	CASE WHEN cost < 100 THEN 'Below 100'
+		WHEN cost BETWEEN 100 AND 500 THEN '100-500'
+		WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
+		ELSE 'Above 1000' END AS cost_range
+FROM gold.dim_products
+)
+
+SELECT 
+	cost_range,
+	count(distinct product_key) AS total_products
+FROM product_segments
+GROUP BY cost_range
+ORDER BY total_products DESC
 
