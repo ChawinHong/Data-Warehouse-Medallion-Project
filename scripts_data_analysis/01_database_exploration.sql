0b664818af8a732a30f1058a13dfc4009e78a0ba
Chawin Gump <168575437+ChawinHong@users.noreply.github.com>
2025-12-28 13:30:45 +0700

    Update 01_database_exploration.sql

--- scripts_data_analysis/01_database_exploration.sql
+++ scripts_data_analysis/01_database_exploration.sql
@@ -1,1 +1,8 @@
+--1) Database Exploration
 
+-- Explore all objects in the database
+SELECT * FROM INFORMATION_SCHEMA.TABLES
+
+-- Explore all columns in the database
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers'
