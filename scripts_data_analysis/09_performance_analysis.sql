198da4057e912ad87afa037a0be6869db9fcb3c6
Chawin Gump <168575437+ChawinHong@users.noreply.github.com>
2025-12-28 13:35:51 +0700

    Add performance analysis SQL script
    Added a SQL script for analyzing yearly product performance by comparing current sales to average sales and previous year's sales.

--- scripts_data_analysis/09_performance_analysis.sql
+++ scripts_data_analysis/09_performance_analysis.sql
@@ -1,1 +1,37 @@
+-- Performance Analysis
+
+/*
+Analyze the yearly performance of products by comparing their sales
+to both the average sales performance of the product and the previous year's sales
+*/
+
+
+WITH yearly_product_sales AS(
+SELECT 
+	YEAR(f.order_date) as order_year,
+	p.product_name,
+	SUM(f.sales_amount) as current_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+	ON f.product_key = p.product_key
+WHERE f.order_date IS NOT NULL
+GROUP BY YEAR(f.order_date), p.product_name
+)
+
+SELECT 
+	order_year,
+	product_name,
+	current_sales,
+	AVG(current_sales) OVER (partition by product_name) AS avg_sales,
+	current_sales - AVG(current_sales) OVER (PARTITION BY product_name) as diff_avg,
+	CASE WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) > 0 THEN 'Above avg'
+		WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) < 0 THEN 'Below avg'
+		ELSE 'avg' END AS avg_change,
+	-- Year-over-year analysis
+	LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) as py_sales,
+	current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) as diff_py,
+	CASE WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increase'
+		WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decrease'
+		ELSE 'No Change' END AS py_change
+FROM yearly_product_sales
 
